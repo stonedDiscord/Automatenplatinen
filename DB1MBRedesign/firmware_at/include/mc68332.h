9ec75d63b9a8b21ff47fbd99f2ca35c1d85a97f5
stonedDiscord <Tukz@gmx.de>
2026-04-26 15:25:57 +0200

    move addresses

---
+++ DB1MBRedesign/firmware_at/include/mc68332.h
@@ -0,0 +1,10 @@
+// MC68332 CPU32 register addresses
+#define SIMCR           0x00FFFA00
+#define SYNCR           0x00FFFA04
+#define SYPCR           0x00FFFA20
+#define CSPAR0          0x00FFFA44
+#define CSPAR1          0x00FFFA46
+#define CSBARBT         0x00FFFA48
+#define CSORBT          0x00FFFA4A
+#define CSBAR0          0x00FFFA4C
+#define CSOR0           0x00FFFA4E
--- DB1MBRedesign/firmware_at/src/main.cpp
+++ DB1MBRedesign/firmware_at/src/main.cpp
@@ -9,6 +9,8 @@
 #include "bdm.h"
 #include "rtc.h"
 
+#include "mc68332.h"
+
 /* 
  * PIN MAPPINGS
  * ----------------------------------
@@ -21,17 +23,6 @@
  * INTRUSION:
  *   PROBE: PB1 (Pulsed), SENSE: PD5 (T1 Input)
  */
-
-// MC68332 CPU32 register addresses
-#define SIMCR           0x00FFFA00
-#define SYNCR           0x00FFFA04
-#define SYPCR           0x00FFFA20
-#define CSPAR0          0x00FFFA44
-#define CSPAR1          0x00FFFA46
-#define CSBARBT         0x00FFFA48
-#define CSORBT          0x00FFFA4A
-#define CSBAR0          0x00FFFA4C
-#define CSOR0           0x00FFFA4E
 
 // System state machine
 typedef enum {
