9c15fb44c52e5178239fd81498fa869e9dbfb001
stonedDiscord <Tukz@gmx.de>
2026-05-13 14:16:14 +0200

    b script

--- DB1MBRedesign/firmware/build.sh
+++ DB1MBRedesign/firmware/build.sh
@@ -1,1 +1,5 @@
-vasm_m68k_mot -mcpu32 -Fbin main.s -o init.bin
+#!/bin/bash
+m68k-elf-as -mcpu=cpu32 main.s -o main.o
+m68k-elf-objcopy -O binary main.o init_raw.bin
+dd if=init_raw.bin of=init.bin bs=1 skip=4032
+rm main.o init_raw.bin
